307642f4644546415717a9edc1f0ee0e672a7016
Mapachana <anabuenrua99@gmail.com>
2018-11-04 16:45:35 +0100

    ED: Añado ejercicio 3, que es  basicamente las funciones union, interseccion y complementario como operadores sobrecargados (+,* y / respectuvamente). Ha sido renombrar las funciones del ejercicio 1, pero bueno, ya que lo  ha sugerido @jruib lo dejo por aqui

---
+++ C1/ED/Ejercicios/ejercicio3set.cpp
@@ -0,0 +1,73 @@
+//By Mapachana
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+//Unión de dos conjuntos
+set<int> operator+(const set<int>& a, const set<int>& b){
+    set<int> resultado = a;
+    set<int>::iterator itb;
+    for (itb = b.begin(); itb != b.end(); ++itb)
+        if (a.find(*itb) == a.end())
+            resultado.insert(*itb);
+
+    return resultado;
+}
+
+//Intersección de dos conjuntos
+set<int> operator*(const set<int>& a, const set<int>& b){
+    set<int> resultado;
+    set<int>::iterator ita;
+    for (ita = a.begin(); ita != a.end(); ++ita)
+        if (b.find(*ita) != b.end())
+            resultado.insert(*ita);
+
+    return resultado;
+}
+
+//Complementario, a-b: devuelve los elementos de a que no estan en b
+set<int> operator/(const set<int>& a, const set<int>& b){
+    set<int> resultado;
+    set<int>::iterator ita;
+    for (ita = a.begin(); ita != a.end(); ++ita)
+        if (b.find(*ita) == b.end())
+            resultado.insert(*ita);
+
+    return resultado;
+}
+
+//Sobrecarga del operador de salida para conjuntos
+ostream& operator<<(ostream& os, const set<int>& a){
+    set<int>::iterator ita;
+    for (ita = a.begin(); ita != a.end(); ++ita)
+        os << *ita << " ";
+    os << endl;
+    return os;
+}
+
+int main(){
+    set<int> a;
+    a.insert(1);
+    a.insert(4);
+    a.insert(2);
+    a.insert(17);
+
+    set<int> b;
+    b.insert(2);
+    b.insert(1);
+    b.insert(5);
+    b.insert(23);
+
+    cout << "Conjunto a:" << endl;
+    cout << a << endl;
+    cout << "Conjunto b:" << endl;
+    cout << b << endl;
+    cout << "Union de a y b:" << endl;
+    cout << a+b << endl;
+    cout << "Interseccion de a y b:" << endl;
+    cout << a*b << endl;
+    cout << "Elementos que estan en a y no en b:" << endl;
+    cout << a/b << endl;
+
+}
