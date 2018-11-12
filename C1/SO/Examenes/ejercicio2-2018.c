a21c1e09c878cc59092ea8dfc05694782e104159
Asmilex <andresmm@outlook.com>
2018-11-12 20:01:20 +0100

    SO: cambiado ejercicio 2 del examen de SO de hoy
    Hemos estado testeando el programa, y parece que da fallos, Sergio. Estaba metiendo un carácter de más, y la comprobación fallaba. Es un caso similar al de mi código. A lo mejor está relacionado con los pipes sin nombre?
    
    Hemos estado echando un ojo al de Antonio, y este sí funciona. Es una estructura muy similar al del otro. Así que, con su permiso, lo subimos. Vaya examencico

--- C1/SO/Examenes/ejercicio2-2018.c
+++ C1/SO/Examenes/ejercicio2-2018.c
@@ -1,150 +1,116 @@
-// Quijano Rey - Sergio
-// Doble Grado Ingenieria Informatica Matematicas
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <signal.h>
-#include <errno.h>
-#include <sys/types.h>
+#include <sys/types.h>	
 #include <sys/wait.h>
+#include <unistd.h>		
+#include <sys/stat.h>
+#include <fcntl.h>		
+#include <errno.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 
 #define NEJ 60
 #define NSALIDA 1024
-static int acabado = 1;
 
-static void handler (int signum){
-	printf("Se ha recibido la señal %i\n");
-	acabado = 0;
+//Manejador para SIGINT
+void sa_handler_SIGINT(int signum){
+	wait(NULL);
+	unlink("FIFO");
+	printf("Finalizamos programa.\n");
+	exit(EXIT_FAILURE);
 }
 
 int main(int argc, char * argv[]){
-	extern int errno;
-	char current_salida[NSALIDA];
-	char old_salida[NSALIDA] = "\0";
 
-	// Añado un manejador para señales para SIGINT
+	char salida[NSALIDA], salida_anterior[NSALIDA];
 	struct sigaction sa;
-	sa.sa_handler = handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if(sigaction(SIGINT, &sa, NULL) == -1){
-		perror("Error cambiando el manejador de señales\n");
-		exit(EXIT_FAILURE);
-	}
+	pid_t PID;
+	int estado;
+	int fd;
+	int leido;
+	char separacion[2]="\n";
+	int fin;
 
-	// Depuro la entrada de argumentos
+	//Comprobamos argumentos
 	if(argc != 2){
-		perror("Error! Argumentos invalidos\n");
-		perror("Modo de uso: ./miwatch <path>\n");
+		printf("Error en el número de argumentos.\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	//Cambiamos acción de SIGINT (Ctrl+C)
+	sa.sa_handler = sa_handler_SIGINT;
+	if(sigaction(SIGINT, &sa, NULL)<0){
+		perror("Error en sigaction.\n");
 		exit(EXIT_FAILURE);
 	}
 
-
-	// Ejecuto el numero de veces necesarias el comando
-	for(int i = 0; i < NEJ && acabado != 0; i++){
-		// Creo un cauce sin nombre
-		int cauce[2];
-		if(pipe(cauce) == -1){
-			perror("Error creando cauce sin nombre\n");
-			exit(EXIT_FAILURE);
-		}
-		
-		// Creo un hijo
-		int pid = fork();
-		if(pid < 0){
-			perror("Error creando un hijo\n");
-			exit(EXIT_FAILURE);
-		}
-
-		if(pid == 0){//HIJO
-			// El hijo solo escribe
-			if(close(cauce[0]) == -1){
-				perror("Error cerrando el cauce de entrada en el hijo\n");
-				exit(EXIT_FAILURE);
-			}
-
-			// Conecto la salida estandar con el cauce
-			if(close(STDOUT_FILENO) == -1){
-				perror("Error cerrando la salida\n");
-				exit(EXIT_FAILURE);
-			}
-
-			// Conecto la salida al cauce
-			if(dup2(cauce[1], STDOUT_FILENO) == -1){
-				perror("Error redireccionando la salida al cauce\n");
-				exit(EXIT_FAILURE);
-			}
-
-			// Ejecuto el comando
-			int errcode = execlp("ls", "ls", argv[1], NULL);
-
-			// Manejo el error del ls
-			if(errcode == -1){
-				perror("Error ejecutando el comando\n");
-				printf("Valor de errno: %i\n", errno);
-
-				// Devuelvo al padre la condicion de fallo
-				exit(errno);
-			}
-			
-			// Cierro el cauce de salida
-			if(close(cauce[1]) == -1){
-				perror("Error en cierre final del cauce del hijo\n");
-				exit(EXIT_FAILURE);
-			}
-		}else{//PADRE
-			// El padre solo lee
-			if(close(cauce[1]) == -1){
-				perror("Error cerrando el cauce de salida del padre\n");
-				exit(EXIT_FAILURE);
-			}
-
-			// Espero a que termine la ejecución del hijo
-			int status;
-			waitpid(pid, &status, 0);
-			
-			// Compruebo el estado de la salida del hijo
-			if(WIFEXITED(status) == 0){	// Ha salido mal
-				perror("Mi hijo ha acabado mal\n");
-				acabado = 0;
-
-				// Muestro por pantalla el valor de ls si me es posible
-				if(read(cauce[0], current_salida, NSALIDA) == -1){
-					printf("No puedo mostrar la salida cuando ha fallado el hijo\n");
-				}else{
-					printf("Valor de salida cuando falla el hijo:\n%s\n", current_salida);
-				}	
-			}
-
-			// Leo del cauce en el array
-			if(read(cauce[0], current_salida, NSALIDA) == -1){
-				perror("Error leyendo del cauce la salida de ls\n");
-				exit(EXIT_FAILURE);
-			}
-
-			// Muestro lo que se ha leido por pantalla
-			printf("Contenido leido:\n%s\n", current_salida);
-
-			// Cierro el cauce de entrada
-			if(close(cauce[0]) == -1){
-				perror("Error en el cierre final del padre\n");
-				exit(EXIT_FAILURE);
-			}
-			
-		}
-
-		// Compruebo que las salidas coinciden
-		if(memcmp(current_salida, old_salida, NSALIDA) != 0 && strcmp(old_salida, "\0") != 0){
-			perror("Dos ejecuciones no coinciden!\n");
-			acabado = 0;
-		}
-
-		// Paralizo la ejecucion del programa 1 segundo
-		sleep(1);
+	//Creamos FIFO intermediario
+	umask(0);
+	unlink("FIFO");
+	if(mkfifo("FIFO", 0666)<0){
+		perror("mkfifo");
+		exit(EXIT_FAILURE);
+	}
+	
+	//Abrimos FIFO
+	if((fd=open("FIFO", O_RDWR))<0){
+		perror("open");
+		exit(EXIT_FAILURE);
 	}
 
-	// Toda la ejecucion ha salido OK
+	//Ejecuciones de ls
+	fin = 0;
+	for(int i=0; i<NEJ && !fin; i++){
+		if((PID=fork())<0){
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (!PID){ //PROCESO HIJO
+			if(dup2(fd, STDOUT_FILENO)<0){
+				perror("dup2");
+				exit(EXIT_FAILURE);
+			}
+			if(execlp("ls", "ls", argv[1], NULL)<0){
+				perror("ls");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else{ //PROCESO PADRE
+			
+			wait(&estado);
+			if(estado != EXIT_SUCCESS){ //Comprobamos estado del hijo
+				exit(EXIT_FAILURE);
+			}
+
+			strcpy(salida_anterior, salida);
+			if((leido=read(fd, salida, NSALIDA))<0){
+				perror("read");
+				exit(EXIT_FAILURE);
+			}
+			
+			if(write(STDOUT_FILENO, salida, leido)<leido){
+				perror("write");
+				exit(EXIT_FAILURE);
+			}
+
+			if(write(STDOUT_FILENO, separacion, 2)<2){
+				perror("write");
+				exit(EXIT_FAILURE);
+			}
+			
+			if(i>0){
+				if(strcmp(salida_anterior, salida)!=0){
+					printf("Se ha detectado cambios en el ls. Saliendo...");
+					fin = 1;
+				}
+			}
+		}
+		sleep(1);	
+	}
+	
+	//Cerramos y eliminamos FIFO
+	close(fd);
+	unlink("FIFO");	
 	exit(EXIT_SUCCESS);
 }
