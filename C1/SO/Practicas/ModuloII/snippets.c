a6d0b6d547d51fa8f20feaec1ad39f6a62690f37
Asmilex <andresmm@outlook.com>
2018-11-12 11:46:37 +0100

    SO: empezado snippets

---
+++ C1/SO/Practicas/ModuloII/snippets.c
@@ -0,0 +1,13 @@
+// Todos los includes 
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/mman.h>
