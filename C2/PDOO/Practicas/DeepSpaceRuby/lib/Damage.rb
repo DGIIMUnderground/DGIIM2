54a6ed6cb3ea9781d9d14d2093592ccf881c580e
Juan Antonio <juanantoniovr98@gmail.com>
2019-03-25 18:21:22 +0100

    PDOO: Añado el adjust que no sé qué narices hice en su tiempo pero se ve que un día me lo zumbé xD

--- C2/PDOO/Practicas/DeepSpaceRuby/lib/Damage.rb
+++ C2/PDOO/Practicas/DeepSpaceRuby/lib/Damage.rb
@@ -23,13 +23,47 @@ module Deepspace
     end
     
     def adjust(w,s)
+      if @weapons==nil
+        return Damage.newNumericWeapons(w.length,s.length)
+        return Damage.newNumericWeapons([@nWeapons, w.length].min,[@nShields, s.length].min)
+      else
+        vec=[]
+        w.each { |weapon|  
+          vec.push(weapon.type)
+        w.each{ |arma|
+          vec.push(arma.type)
+        }
+        return Damage.newSpecificWeapons(vec, s.length)
+        tipos=[]
+        for i in (0..[vec.count(WeaponType::LASER),@weapons.count(WeaponType::LASER)].min-1) do
+          tipos.push(WeaponType::LASER)
+        end
+        for i in (0..[vec.count(WeaponType::MISSILE),@weapons.count(WeaponType::MISSILE)].min-1) do
+          tipos.push(WeaponType::MISSILE)
+        end
+        for i in (0..[vec.count(WeaponType::PLASMA),@weapons.count(WeaponType::PLASMA)].min-1) do
+          tipos.push(WeaponType::PLASMA)
+        end
+
+
+        return Damage.newSpecificWeapons(tipos,[@nShields, s.length].min)
+      end
     end
+
+
     
     def adjustShieldBoosters(s)
       return [s.length,@nShields].min
     end
     
     def discardWeapon(w)
+      if @weapons!=nil
+        @weapons.delete(w.type)
+      else
+        if @nWeapons>0
+        @nWeapons-=1
+        end
+      end 
     end
     
     def discardShieldBooster
